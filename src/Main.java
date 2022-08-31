2c6ef0684f9c6580652e45d95486451fa387fa3e
ruslanzverkov <createmed@mail.ru>
2022-08-31 17:28:22 +0300

    12

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       Integer[] value=new Integer[]{3,4};
+       changeValue(value);
+        for (int i = 0; i < value.length; i++) {
+            System.out.println(value[i]);
+        }
+
+
+    }
+
+    public static Integer[] changeValue(Integer[] value) {
+        value[0]=99;
+         return value;
     }
 }
